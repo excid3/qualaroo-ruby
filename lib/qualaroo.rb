d785f865c263bde3030f0722e6b8483377ef217b
Chris Oliver <excid3@gmail.com>
2014-10-20 10:46:24 -0500

    Add method to retrieve all the responses from a nudge

--- lib/qualaroo.rb
+++ lib/qualaroo.rb
@@ -11,9 +11,21 @@ module Qualaroo
       @auth = {username: key, password: secret}
     end
 
-    def responses(survey_id, options={})
-      options.merge!(basic_auth: @auth)
-      self.class.get "/nudges/#{survey_id}/responses.json", options
+    def responses(nudge_id, query={}, options={})
+      options.merge!(basic_auth: @auth, query: query)
+      self.class.get("/nudges/#{nudge_id}/responses.json", options)
+    end
+
+    def all_responses(nudge_id, query={}, options={})
+      offset = 0
+      all_responses = []
+
+      while (page_responses = responses(nudge_id, query.merge(offset: offset), options)) && page_responses.any?
+        all_responses += page_responses
+        offset += 500
+      end
+
+      all_responses
     end
   end
 end
